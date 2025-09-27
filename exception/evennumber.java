08f5353a775da2c38ba69619f5d2c48d737e4c73
Murali <muralisurya2004@gmail.com>
2025-09-27 23:08:59 +0530

    Create evennumber.java

---
+++ exception/evennumber.java
@@ -0,0 +1,23 @@
+package customisedexception;
+import java.util.Scanner;
+public class mainmethod 
+{
+public static void main(String [] args)
+{
+Scanner scn = new Scanner(System.in);
+System.out.println("enter num");
+int num = scn.nextInt();
+
+if(num%2==0)
+{
+	System.out.println(" even number " +num);
+throw new  EvenNumberException("even number")	;
+}
+else
+{
+	System.out.println("not an even number " +num);
+	throw new NotAnEvenNumberException(" not an even number");
+	
+}
+}
+}
