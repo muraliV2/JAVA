f7ebc567c61a65f3b2f88a1c0affa6c3b1a7c5a4
Murali <muralisurya2004@gmail.com>
2025-09-29 01:35:27 +0530

    Create parse.java

---
+++ wrapperclass/parse.java
@@ -0,0 +1,15 @@
+package wrapper;
+
+public class autoboxing 
+{
+public static void main(String [] args)
+{
+String str = "98";
+int num3 = Integer.parseInt(str);
+System.out.println(num3*3);
+
+}
+}
+
+converting string into primitive type 
+i make the string value 98 into primitive int type
