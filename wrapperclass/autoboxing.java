781df41d235f05b426fe7012b2f1050b810a87e5
Murali <muralisurya2004@gmail.com>
2025-09-29 01:29:07 +0530

    Create autoboxing.java

---
+++ wrapperclass/autoboxing.java
@@ -0,0 +1,15 @@
+package wrapper;
+
+public class autoboxing 
+{
+public static void main(String [] args)
+{
+int a = 10;
+Integer num = a;
+System.out.println(a);
+
+System.out.println("auto unboxing");
+
+
+}
+}
