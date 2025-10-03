f2ae5a202034f53e3bdf004e9a5d0d284bf47718
Murali <muralisurya2004@gmail.com>
2025-10-04 00:02:28 +0530

    Create removemethod.java

---
+++ Collections/arraylist/removemethod.java
@@ -0,0 +1,18 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class arraylist 
+{
+public static void main(String [] args)
+{
+ArrayList<String> name = new ArrayList<>();
+name.add("hello");
+name.add("world");
+name.add("dybala");
+name.add("messi");
+name.add("neymar");
+name.remove(2);
+System.out.println(name);
+}
+}
