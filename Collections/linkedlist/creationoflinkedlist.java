ce39017cad2ac5d74f05a7130dd8b3ebff5a918d
Murali <muralisurya2004@gmail.com>
2025-10-04 00:17:42 +0530

    Create creationoflinkedlist.java

---
+++ Collections/linkedlist/creationoflinkedlist.java
@@ -0,0 +1,19 @@
+package collections;
+
+import java.util.LinkedList;
+
+import company.intern;
+
+public class linkedlist 
+{
+public static void main(String [] args)
+{
+LinkedList<Integer> num = new LinkedList<>();
+num.add(1);
+num.add(2);
+num.add(3);
+num.add(4);
+
+System.out.println(num);
+}
+}
