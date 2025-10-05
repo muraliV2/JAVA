3710d750c7755b40835dcf2546ed07ca90ddedc1
Murali <muralisurya2004@gmail.com>
2025-10-05 23:32:38 +0530

    Create isempty().java

---
+++ Collections/linkedlist/isempty().java
@@ -0,0 +1,28 @@
+package list;
+
+import java.util.LinkedList;
+
+
+
+
+public class arraylist 
+{
+public static void main(String [] args)
+{
+LinkedList<Integer> ll = new LinkedList<Integer>();
+ll.add(00);
+ll.add(10);
+ll.add(20);
+ll.add(30);
+ll.add(40);
+ll.add(50);
+ll.add(50);
+ll.add(2,30);
+
+
+System.out.println(ll.isEmpty());
+
+}
+}
+it returns boolean value with respect to the list 
+  if the list id empty IT PROVIDES true if the list is not empty returns false
