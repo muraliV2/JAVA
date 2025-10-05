b2e15c4e4e909876a513ae5aa28bbc98d9ee8399
Murali <muralisurya2004@gmail.com>
2025-10-05 23:25:58 +0530

    Create remove()& remove(index,).java

---
+++ Collections/linkedlist/remove()& remove(index,).java
@@ -0,0 +1,51 @@
+package list;
+
+import java.util.LinkedList;
+
+
+
+
+public class arraylist 
+{
+public static void main(String [] args)
+{
+LinkedList<Integer> ll = new LinkedList<Integer>();
+ll.add(00);
+ll.add(10);
+ll.add(20);
+ll.add(30);
+ll.add(40);
+ll.add(50);
+ll.add(2,30);
+ll.remove();
+
+System.out.println(ll);
+
+}
+}
+
+package list;
+
+import java.util.LinkedList;
+
+
+
+
+public class arraylist 
+{
+public static void main(String [] args)
+{
+LinkedList<Integer> ll = new LinkedList<Integer>();
+ll.add(00);
+ll.add(10);
+ll.add(20);
+ll.add(30);
+ll.add(40);
+ll.add(50);
+ll.add(2,30);
+ll.remove(6);
+
+System.out.println(ll);
+
+}
+}
