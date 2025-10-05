5084ee1783cbf708e686eb92215c952839d259bf
Murali <muralisurya2004@gmail.com>
2025-10-05 23:38:24 +0530

    Create contains().java

---
+++ Collections/linkedlist/contains().java
@@ -0,0 +1,28 @@
+package list;
+
+import java.util.LinkedList;
+
+
+
+
+public class arraylist 
+{
+public static void main(String [] args)
+{
+LinkedList<Integer> ll = new LinkedList<Integer>();
+ll.add(00);
+ll.add(10);
+ll.add(20);
+ll.add(30);
+ll.add(40);
+ll.add(50);
+ll.add(50);
+ll.add(2,30);
+
+
+System.out.println(ll.contains(50));
+
+}
+}
+if the value contains in the list it returns true 
+if not contains it returns false
