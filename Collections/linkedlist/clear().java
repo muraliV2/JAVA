4636d44f4dd79118b6d2ec91ab813668d775142e
Murali <muralisurya2004@gmail.com>
2025-10-05 23:39:47 +0530

    Create clear().java

---
+++ Collections/linkedlist/clear().java
@@ -0,0 +1,27 @@
+package list;
+
+import java.util.LinkedList;
+
+
+
+
+public class arraylist 
+{
+public static void main(String [] args)
+{
+LinkedList<Integer> ll = new LinkedList<Integer>();
+ll.add(00);
+ll.add(10);
+ll.add(20);
+ll.add(30);
+ll.add(40);
+ll.add(50);
+ll.add(50);
+ll.add(2,30);
+ll.clear();
+
+System.out.println(ll);
+
+}
+}
+it clears entire element in the list 
