ba1e346fc17073b18b59fed0e008ac0d7b678322
Murali <muralisurya2004@gmail.com>
2025-10-14 00:20:24 +0530

    Create searchinstack.java

---
+++ Collections/searchinstack.java
@@ -0,0 +1,23 @@
+package stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class stacksearch 
+{
+public static void main(String [] args)
+{
+	Stack sta = new Stack();
+	sta.add(10);
+	sta.add("messi");
+	sta.add(7);
+	sta.add("ronaldo");
+	sta.add(66);
+	sta.add("arnold");
+	
+	System.out.println(sta.get(5));
+	
+
+}
+}
