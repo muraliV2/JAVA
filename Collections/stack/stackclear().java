718c22a01dc7a92cfe30d6be50859be7fa893c68
Murali <muralisurya2004@gmail.com>
2025-10-14 00:22:05 +0530

    Create stackclear().java

---
+++ Collections/stack/stackclear().java
@@ -0,0 +1,23 @@
+package stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class stackdelete 
+{
+public static void main(String [] args)
+{
+	Stack sta = new Stack();
+	sta.add(10);
+	sta.add("messi");
+	sta.add(7);
+	sta.add("ronaldo");
+	sta.add(66);
+	sta.add("arnold");
+	sta.clear();
+	System.out.println(sta);
+	
+
+}
+}
