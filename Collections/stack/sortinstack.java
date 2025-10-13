4118108124033ce14870d5bc85727b151696b9b5
Murali <muralisurya2004@gmail.com>
2025-10-14 00:30:47 +0530

    Create sortinstack.java

---
+++ Collections/stack/sortinstack.java
@@ -0,0 +1,20 @@
+package stack;
+
+import java.util.Collections;
+import java.util.Stack;
+
+public class sort 
+{
+public static void main(String [] args)
+{
+	Stack sta = new Stack();
+	sta.add(10);
+	sta.add(4545);
+	sta.add(7);
+	sta.add(454);
+	sta.add(66);
+	sta.add(1);
+	System.out.println(Collections.sort(sta));
+	
+}
+}
