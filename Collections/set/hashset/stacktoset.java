24fbcbc2ef958d9f351e4593a22e5af940839c3f
Murali <muralisurya2004@gmail.com>
2025-10-18 00:13:48 +0530

    Create stacktoset.java

---
+++ Collections/set/hashset/stacktoset.java
@@ -0,0 +1,32 @@
+package hashset;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+public class linkedlisttoset 
+{
+public static void main(String [] args)
+{
+Stack<Integer> list = new Stack<Integer>();
+list.add(10);
+list.add(20);	
+list.add(30);	
+list.add(40);	
+list.add(50);	
+list.add(60);	
+list.add(70);	
+list.add(80);	
+list.add(90);	
+list.add(100);	
+
+System.out.println(list);
+
+System.out.println("converting this arraylist into set");
+Set<Integer> set = new HashSet<Integer>(list);
+System.out.println(set);
+}
+}
