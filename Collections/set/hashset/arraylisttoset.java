2d0f1157e429e4ba8b14965d82e3d009503ba5a7
Murali <muralisurya2004@gmail.com>
2025-10-18 00:01:31 +0530

    Create arraylisttoset.java

---
+++ Collections/set/hashset/arraylisttoset.java
@@ -0,0 +1,30 @@
+package hashset;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class linkedlisttoset 
+{
+public static void main(String [] args)
+{
+List<Integer> list = new ArrayList<Integer>();
+list.add(10);
+list.add(20);	
+list.add(30);	
+list.add(40);	
+list.add(50);	
+list.add(60);	
+list.add(70);	
+list.add(80);	
+list.add(90);	
+list.add(100);	
+
+System.out.println(list);
+
+System.out.println("converting this arraylist into set");
+Set<Integer> set = new HashSet<Integer>(list);
+System.out.println(set);
+}
+}
