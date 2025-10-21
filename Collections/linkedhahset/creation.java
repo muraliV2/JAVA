de133856ca73fe56a0a7cd0f690f77dd3db7c7cc
Murali <muralisurya2004@gmail.com>
2025-10-21 23:27:12 +0530

    Create creation.java

---
+++ Collections/linkedhahset/creation.java
@@ -0,0 +1,21 @@
+package linkedhashset;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class creation {
+	public static void main(String [] args)
+	{
+	Set rt = new LinkedHashSet();
+	rt.add("hello");
+	rt.add("world");
+	rt.add("long ball");
+	rt.add("kio");
+	rt.add(12);
+	rt.add('a');
+	System.out.println(rt);
+	
+	}
+		
+
+}
