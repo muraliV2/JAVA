bbaed0e3a7d7e2e91fca06bb91ac3b0bb276e19c
Murali <muralisurya2004@gmail.com>
2025-11-03 23:47:51 +0530

    Create creation.java

---
+++ hashmap/creation.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class SimpleHashMap {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put(1, "Murali");
+        map.put(2, "ECE");
+        map.put(3, "Java");
+        System.out.println(map);
+    }
+}
