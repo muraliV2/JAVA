32b48f403616bce0b3a74cee7c6bf10f3354196a
Murali <muralisurya2004@gmail.com>
2025-11-04 00:38:19 +0530

    Create creation.java

---
+++ Collections/hashmap/creation.java
@@ -0,0 +1,24 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class creation 
+{
+public static void main(String [] args)
+{
+System.out.println("creation of hasmap");	
+Map map = new HashMap<>();
+map.put(1, 10);
+map.put('a', 'v');
+map.put("hi", "hello");
+map.put(null, 3);
+System.out.println(map);
+
+
+
+}
+
+}
+
+
