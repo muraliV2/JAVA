5754b192e485799c7e1893f4dbf2eda83ffb9e42
Murali <muralisurya2004@gmail.com>
2025-12-18 23:05:22 +0530

    Create clashroyale.java

---
+++ generalization/clashroyale.java
@@ -0,0 +1,23 @@
+package generalization;
+
+public class clashroyale  extends games{
+
+	@Override
+	public void name() {
+		System.out.println("clash royale");
+		
+	}
+
+	@Override
+	public void noofplayers() {
+		System.out.println("2v2");
+		
+	}
+
+	@Override
+	public void famousplay() {
+		System.out.println("prathish jod & sailesh");
+		
+	}
+
+}
