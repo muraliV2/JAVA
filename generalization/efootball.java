a8c16711d84e1bd033c6b43fb5166685d14de705
Murali <muralisurya2004@gmail.com>
2025-12-18 22:53:21 +0530

    Create efootball.java

---
+++ generalization/efootball.java
@@ -0,0 +1,25 @@
+package generalization;
+
+public class efootball  extends games
+{
+
+	@Override
+	public void name() {
+		System.out.println("efootball");
+		
+	}
+
+	@Override
+	public void noofplayers() {
+		System.out.println("1v1");
+		
+	}
+
+	@Override
+	public void famousplay() {
+		System.out.println("pratzz");
+		
+	}
+
+
+}
