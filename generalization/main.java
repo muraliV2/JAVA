d9faf197ac2e4b23efb4e5a9acee39c441cdccd0
Murali <muralisurya2004@gmail.com>
2025-12-18 23:06:02 +0530

    Create main.java

---
+++ generalization/main.java
@@ -0,0 +1,13 @@
+package generalization;
+
+public class main 
+{
+public static void main(String [] args)
+{
+System.out.println("it is a generalization program");
+help h1 = new help();
+h1.get(new efootball());
+h1.get(new clashroyale());
+h1.get(new coc());
+}
+}
