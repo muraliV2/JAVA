d530a6a0fdec437e9545026b96a55230e383a08f
Murali <muralisurya2004@gmail.com>
2025-12-19 22:50:32 +0530

    Create main.java

---
+++ specilization/main.java
@@ -0,0 +1,14 @@
+package specilizationnew;
+
+
+
+public class main 
+{
+public static void main(String [] args)
+{
+	help h1 = new help();
+	h1.get(new pep());
+	h1.get(new klopp());
+	h1.get(new doncarlo());
+	}
+}
