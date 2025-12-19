902650f8f5b1f9d54202ebb7945268cdf6a15a0a
Murali <muralisurya2004@gmail.com>
2025-12-19 22:49:38 +0530

    Create klopp.java

---
+++ specilization/klopp.java
@@ -0,0 +1,24 @@
+package specilizationnew;
+
+public class klopp extends manager
+{
+
+	@Override
+	public void team() {
+		System.out.println("he managed liverpool");
+		
+	}
+
+	@Override
+	public void league() {
+		System.out.println("in premier league");
+		
+	}
+
+	@Override
+	public void playstyle() {
+		System.out.println("famous for playing quick counter");
+		
+	}
+
+}
