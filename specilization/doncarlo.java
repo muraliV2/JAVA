f20a0111a9863397c8eb23c6095f54a2177d5f7c
Murali <muralisurya2004@gmail.com>
2025-12-19 22:49:58 +0530

    Create doncarlo.java

---
+++ specilization/doncarlo.java
@@ -0,0 +1,22 @@
+package specilizationnew;
+
+public class doncarlo extends manager{
+
+	@Override
+	public void team() {
+		System.out.println("currently managing brazil");
+		
+	}
+
+	@Override
+	public void league() {
+		System.out.println("managing an national team");
+		
+	}
+
+	@Override
+	public void playstyle() {
+		System.out.println("quick counter + longball");
+	}
+
+}
