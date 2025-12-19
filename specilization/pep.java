8dae83f868c5ad47c61d6ac31144909eddcfb42a
Murali <muralisurya2004@gmail.com>
2025-12-19 22:49:16 +0530

    Create pep.java

---
+++ specilization/pep.java
@@ -0,0 +1,22 @@
+package specilizationnew;
+
+public class pep extends manager{
+
+	@Override
+	public void team() {
+		System.out.println("coaching manchester city");
+		
+	}
+
+	@Override
+	public void league() {
+		System.out.println("playing in premier league");
+		
+	}
+
+	@Override
+	public void playstyle() {
+		
+	}
+
+}
