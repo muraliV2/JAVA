767ede2cde85ccb4fb8ae8a2376974b88965acf2
Murali <muralisurya2004@gmail.com>
2026-01-24 00:08:36 +0530

    Create lambda2.java

---
+++ java8/lambda2.java
@@ -0,0 +1,23 @@
+package java8;
+
+public interface thetre 
+{
+ int add(int i,int j);
+}
+
+package java8;
+
+public class adf implements thetre {
+
+    public static void main(String[] args) {
+
+       thetre th = (int i,int j) ->
+       {
+    	 return i*j  ;
+    	   
+       };
+       
+       int result =th.add(10, 7);
+       System.out.println(result);
+    }
+}
