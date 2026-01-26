41c8812c9272940f24dc5651c00ef45c3c41f7bf
Murali <muralisurya2004@gmail.com>
2026-01-26 10:46:14 +0530

    Create streamsort.java

---
+++ java8/streamsort.java
@@ -0,0 +1,58 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class stram 
+{
+public static void main(String [] args)
+{
+List <Integer> li = Arrays.asList(8,4,5,6,7,3,4); 
+
+Stream<Integer> data = li.stream();
+
+
+Stream<Integer> data2 = data.sorted();
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class stram 
+{
+public static void main(String [] args)
+{
+List <Integer> li = Arrays.asList(8,4,5,6,7,3,4); 
+
+Stream<Integer> data = li.stream();
+
+
+Stream<Integer> data2 = data.sorted();
+data2.forEach(n -> System.out.println(n * 2));
+
+
+//Stream<Integer> str = li.stream();
+//
+//
+//
+//long count = str.count();
+//System.out.println(count);
+
+//str.forEach(n ->  System.out.println(n));
+}
+}
+
+
+
+//Stream<Integer> str = li.stream();
+//
+//
+//
+//long count = str.count();
+//System.out.println(count);
+
+//str.forEach(n ->  System.out.println(n));
+}
+}
