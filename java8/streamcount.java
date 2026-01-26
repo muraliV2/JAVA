0bd95db4f2e2ccd802d71f46a78e2efe3c6b0dbd
Murali <muralisurya2004@gmail.com>
2026-01-26 10:40:20 +0530

    Create streamcount.java to count list elements
    Add a Java program to count elements in a list using streams.

---
+++ java8/streamcount.java
@@ -0,0 +1,21 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class stram 
+{
+public static void main(String [] args)
+{
+List <Integer> li = Arrays.asList(1,2,3,4,5,6); 
+
+Stream<Integer> str = li.stream();
+
+
+
+long count = str.count();
+System.out.println(count);
+
+}
+}
