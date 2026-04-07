bf1e078912c37dc42adb1b5b1d098ee4b2c29e48
Murali <muralisurya2004@gmail.com>
2026-04-07 23:06:30 +0530

    Create creation1.java

---
+++ creation1.java
@@ -0,0 +1,16 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class arraylist 
+{
+public static void main(String [] args)
+{
+ArrayList<String> name = new ArrayList<>();
+name.add("hello");
+name.add("world");
+name.add("dybala");
+  name.add("messi");
+System.out.println(name);
+}
+}
