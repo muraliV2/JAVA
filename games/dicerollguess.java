59ae8976009078d130a6cdb793243da874de163d
Murali <muralisurya2004@gmail.com>
2026-04-19 20:44:57 +0530

    Update dicerollguess.java

--- games/dicerollguess.java
+++ games/dicerollguess.java
@@ -12,8 +12,8 @@ int compchoice = rand.nextInt(6)+1;
 int guess = 0;
 while(guess !=compchoice)	
 {
-	System.out.println("guess number between 1 to 6 dicerolls");
-	guess = scn.nextInt();
+System.out.println("guess number between 1 to 6 dicerolls");
+guess = scn.nextInt();
 	if(guess == compchoice)
 	{
 		System.out.println("your guess is correct  : " +guess);
