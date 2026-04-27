e938acf2f7edd846fb25698f6c0b350738f0939a
Murali <muralisurya2004@gmail.com>
2026-04-27 23:23:02 +0530

    Update and rename deserialization.java to deserialization1.java

--- serialization/deserialization1.java
+++ serialization/deserialization1.java
@@ -10,8 +10,7 @@ public class main
 public static void main(String [] args) throws Exception
 {
 a a1 = new a("murali","ECE",20017);
-
-  FileInputStream fis = new FileInputStream("C:\\Users\\mural\\OneDrive\\Desktop\\lo\\java\\gigi.ser");
+FileInputStream fis = new FileInputStream("C:\\Users\\mural\\OneDrive\\Desktop\\lo\\java\\gigi.ser");
 ObjectInputStream ois = new ObjectInputStream(fis);
  a a2 = (a)ois.readObject();
 System.out.println(a2.name);
