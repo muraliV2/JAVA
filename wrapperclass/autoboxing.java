ff7d33ea61981fb9c427e656dc7ade070f1570d7
Murali <muralisurya2004@gmail.com>
2026-05-04 20:06:24 +0530

    Update autoboxing.java

--- wrapperclass/autoboxing.java
+++ wrapperclass/autoboxing.java
@@ -11,6 +11,5 @@ System.out.println(a);
 System.out.println("auto unboxing");
 }
 }
-
 providing primitive  data to object is called boxing 
   if it is automatically done by compiler it is called autoboxing
