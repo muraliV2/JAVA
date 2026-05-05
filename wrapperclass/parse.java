ebff6c399ff24877cec1e01973df006f5b9269da
Murali <muralisurya2004@gmail.com>
2026-05-05 21:35:24 +0530

    Update parse.java

--- wrapperclass/parse.java
+++ wrapperclass/parse.java
@@ -5,7 +5,7 @@ public static void main(String [] args)
 {
 String str = "100";
 int num3 = Integer.parseInt(str);
-System.out.println(num3*3);
+System.out.println(num3*4);
 }
 }
 converting string into primitive type 
