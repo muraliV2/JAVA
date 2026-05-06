9d3ba67c5943ef80e00cb6c783fe264e27545796
Murali <muralisurya2004@gmail.com>
2026-05-06 20:58:40 +0530

    Update threadname.java

--- thread/threadname.java
+++ thread/threadname.java
@@ -10,7 +10,7 @@ System.out.println("id of the usd :" +Thread.currentThread().getName());
 public static void main(String [] args)
 {
 System.out.println(Thread.currentThread().getName());	
-Thread.currentThread().setName("main thread");
+Thread.currentThread().setName("this is main thread");
 System.out.println("id of the main :" +Thread.currentThread().getName());	
 getandset ti = new getandset();
 Thread t1 = new Thread(ti);
