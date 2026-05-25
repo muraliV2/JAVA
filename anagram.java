c50fd78955973a031db93fcb90159154593c1077
Murali <muralisurya2004@gmail.com>
2026-05-25 22:35:16 +0530

    optimised code

--- anagram.java
+++ anagram.java
@@ -7,16 +7,13 @@ public class str
     public static void main(String[] args)
     {
         Scanner scn = new Scanner(System.in);
-
         System.out.println("Enter 1st string:");
         String s1 = scn.next();
         System.out.println("Enter 2nd string:");
         String s2 = scn.next();
-
         s1 = s1.toLowerCase().replaceAll("\\s", "");
         s2 = s2.toLowerCase().replaceAll("\\s", "");
-
-        if(s1.length() != s2.length())
+       if(s1.length() != s2.length())
         {
             System.out.println("Not an Anagram");
         }
