77e7e4c8e69c82af78bd8b2087debcee091f162f
Murali <muralisurya2004@gmail.com>
2026-06-01 22:41:04 +0530

    Update doncarlo.java

--- specilization/doncarlo.java
+++ specilization/doncarlo.java
@@ -4,10 +4,9 @@ public class doncarlo extends manager{
 
 	@Override
 	public void team() {
-		System.out.println("currently managing brazil");
+	System.out.println("currently managing brazil(CFB)");
 		
 	}
-
 	@Override
 	public void league() {
 	System.out.println("managing an brazil National team");
